2f31eb3eb3ab09dfac8808c309496f6fccf371c8
moonjongjs <moonseonjong@naver.com>
2025-09-17 10:22:17 +0900

    NextJS moongoDb Project Deploy

--- src/models/Todo.js
+++ src/models/Todo.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 
 const TodoSchema = new mongoose.Schema({
-  아이디: { type: String, default: null, required: true },
   할일: { type: String, default: null, required: true },
-  완료: { type: Number, default: 0 , required: true},
+  완료: { type: Number, default: 0 },
   기한: { type: String, default: null, required: true },
   등록일: { type: String, default: null , required: true}
 });
